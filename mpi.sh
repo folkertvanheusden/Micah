8126d4bb4ac93b9a70bfd96e4b17ac6e20cbbddc
Folkert van Heusden <mail@vanheusden.com>
2021-09-12 09:08:08 +0200

    wrapper scripts

--- mpi.sh
+++ mpi.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-mpirun --npersocket 1 --hostfile /home/pi/mpi.hosts build/Micah -c 4
+mpirun --npersocket 1 --hostfile /home/pi/mpi.hosts build/Micah -c 4 -T /home/pi/cluster/Micah/tune.dat
---
+++ wrapper.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+
+cd /home/pi/cluster/Micah
+
+/usr/games/polyglot -noini -ec ./mpi.sh

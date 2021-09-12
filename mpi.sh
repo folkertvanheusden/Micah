c7ab204674a55daaf82a076b79d18284b0b1ff21
Folkert van Heusden <mail@vanheusden.com>
2021-09-12 18:36:26 +0200

    helpers

--- mpi.sh
+++ mpi.sh
@@ -1,3 +1,5 @@
 #! /bin/sh
 
-mpirun --npersocket 1 --hostfile /home/pi/mpi.hosts build/Micah -c 4 -T /home/pi/cluster/Micah/tune.dat
+cd /home/pi/cluster/Micah
+
+mpirun --hostfile mpi.hosts build/Micah -c 4 -T tune.dat
